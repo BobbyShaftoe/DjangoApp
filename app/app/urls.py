81ca34e5ad6ff31a2927afaacefc38774e8e8c31
JackShaftoe <tech@halcyonnetworks.com.au>
2016-08-08 20:27:22 +1000

    Added __init__.py 08082016

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,7 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include ('base.urls')),
     url(r'^base/', include ('base.urls')),
     url(r'^admin/', admin.site.urls),
 ]
